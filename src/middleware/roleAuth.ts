37dcb10dc37a901c65387a7dd332f1c311a21b27
William Ferguson <willfergusmusic@gmail.com>
2025-12-16 09:11:53 -0500

    Role fix

--- src/middleware/roleAuth.ts
+++ src/middleware/roleAuth.ts
@@ -5,7 +5,7 @@ import createError from "../utils/createError";
 export const roleAuth = (req: Request, _: Response, next: NextFunction) => {
   const user = (req as any).user as JwtUser;
 
-  if (!user || user.role !== 'admin' || user.role !== 'admin') {
+  if (!user || user.role !== 'admin') {
     return next(createError("Unauthorized", 401, "UNAUTHORIZED"))
   }
   next()
